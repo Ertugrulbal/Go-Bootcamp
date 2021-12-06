25fd20707f75c50932ee23490f9322a2f6c3da6b
Ertuğrul BAL <92356291+Ertugrulbal@users.noreply.github.com>
2021-12-06 13:36:41 +0300

    The reading and implementing of .csv files

--- main.go
+++ main.go
@@ -1,180 +1,52 @@
-// package main
-
-// import (
-// 	"encoding/xml"
-// 	"fmt"
-// 	"io/ioutil"
-// 	"os"
-// )
-
-// // XML to Go
-
-// func main() {
-// 	xmlFile, err := os.Open("Employees.xml")
-// 	if err != nil {
-// 		fmt.Println("Opening file error : ", err)
-// 		return
-// 	}
-// 	defer xmlFile.Close()
-
-// 	xmlData, _ := ioutil.ReadAll(xmlFile)
-
-// 	// Unmarshalling
-// 	var c Company
-// 	// xmlData'ya gelen byte[]'si formatındaki veriyi Company'nin nesne örneğine aktarır/dönüştür.
-// 	xml.Unmarshal(xmlData, &c)
-
-// 	// fmt.Println(c.Persons)
-// 	for i := 0; i < 3; i++ {
-// 		fmt.Println(c.Persons[i])
-// 	}
-
-// }
-
-// type Person struct {
-// 	XMLName   xml.Name `xml:"person"`
-// 	ID        int      `xml:"id"`
-// 	FirstName string   `xml:"firstname"`
-// 	LastName  string   `xml:"lastname"`
-// 	UserName  string   `xml:"username"`
-// }
-
-// type Company struct {
-// 	XMLName xml.Name `xml:"company"`
-// 	Persons []Person `xml:"person"`
-// }
-
-// func (p Person) String() string {
-// 	return fmt.Sprintf("\t ID: %d - FirstName : %s %s -> UserName : %s",
-// 		p.ID, p.FirstName, p.LastName, p.UserName)
-// }
-//*********************************** JSON işlemleri ************************************
 package main
 
 import (
-	"encoding/json"
+	"encoding/csv"
 	"fmt"
+	"log"
 	"os"
 )
 
-// JSON Operations in Go
-
 func main() {
-
-	person := Person{
-		ID:        7,
-		FirstName: "Cihan",
-		LastName:  "Özhan",
-		UserName:  "cihanozhan",
-		Gender:    "Erkek",
-		Name:      Name{Family: "xyz", Personal: "Cihan"},
-		Email: []Email{
-			{ID: 1, Kind: "work", Address: "cihan@deeplab.co"},
-			{ID: 2, Kind: "home", Address: "cihan.ozhan@hotmail.com"},
-		},
-		Interest: []Interest{
-			{ID: 1, Name: "C#"},
-			{ID: 2, Name: "Go"},
-			{ID: 3, Name: "Python"},
-			{ID: 4, Name: "SQL Server"},
-			{ID: 5, Name: "PostgreSQL"},
-		},
+	records, err := ReadData("data.csv")
+	if err != nil {
+		log.Fatal()
 	}
-
-	WriteMessage("Reading Operation Started.")
-
-	WriteMessage("Personal Fullname")
-	WriteStarLine()
-	res := GetPerson(&person)
-	WriteMessage(res)
-	WriteStarLine()
-
-	WriteMessage("\n")
-
-	WriteMessage("Personal Email with Index")
-	WriteStarLine()
-	resEmail := GetPersonEmailAddress(&person, 0)
-	WriteMessage(resEmail)
-	WriteStarLine()
-
-	WriteMessage("\n")
-
-	WriteMessage("Personal Email Object with Index")
-	WriteStarLine()
-	resEmail2 := GetPersonEmail(&person, 0)
-	fmt.Println(resEmail2)
-	WriteStarLine()
-
-	WriteMessage("Reading Operation Ended.")
-	WriteMessage("\n")
-	WriteMessage("Writing Operation Started.")
-	SaveJSON("person.json", person)
-	WriteMessage("Writing operation ended.")
+	for _, record := range records {
+		user := User{
+			FirstName: record[0],
+			LastName:  record[1],
+			Job:       record[2],
+		}
+		fmt.Printf("%s %s is a %s\n", user.FirstName, user.LastName, user.Job)
+	}
 }
 
-// Struct Nesnelerimiz
-
-type Person struct {
-	ID        int
+type User struct {
 	FirstName string
 	LastName  string
-	UserName  string
-	Gender    string
-	Name      Name
-	Email     []Email
-	Interest  []Interest
+	Job       string
 }
 
-type Name struct {
-	Family   string
-	Personal string
-}
-
-type Email struct {
-	ID      int
-	Kind    string
-	Address string
-}
-
-type Interest struct {
-	ID   int
-	Name string
-}
-
-// Function'lar
-
-func GetPerson(p *Person) string {
-	return p.FirstName + " " + p.LastName
-}
-
-func GetPersonEmailAddress(p *Person, ind int) string {
-	return p.Email[ind].Address
-}
-
-func GetPersonEmail(p *Person, ind int) Email {
-	return p.Email[ind]
-}
-
-func WriteMessage(msg string) {
-	fmt.Println(msg)
-}
-
-func WriteStarLine() {
-	fmt.Println("****************")
-}
-
-func SaveJSON(fileName string, key interface{}) {
-	outFile, err := os.Create(fileName)
-	checkError(err)
-	encoder := json.NewEncoder(outFile)
-	err = encoder.Encoder(key)
-	checkError(err)
-	outFile.Close()
-}
-
-func checkError(err error) {
+func ReadData(fileName string) ([][]string, error) {
+	f, err := os.Open(fileName)
 	if err != nil {
-		fmt.Println("Fatal Error : ", err.Error())
-		os.Exit(1)
+		return [][]string{}, err
 	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+
+	//ilk satırı atla kodu.
+	if _, err := r.Read(); err != nil {
+		return [][]string{}, err
+	}
+
+	records, err := r.ReadAll()
+
+	if err != nil {
+		return [][]string{}, err
+	}
+
+	return records, nil
 }
